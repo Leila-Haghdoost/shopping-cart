4eeebf7afecd908503906f1980adcf3b984e0aa3
Leila haghdoost <leilahaghdoost@192-168-1-6.tpgi.com.au>
2019-02-03 16:55:21 +1100

    adding the cart component

--- vue-practice-shop/src/App.vue
+++ vue-practice-shop/src/App.vue
@@ -12,7 +12,9 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-md-5"></div>
+			<div class="col-md-5 my-5">
+				<cart :items="cart"></cart>
+			</div>
 		</div>
 	</div>
 </template>
@@ -20,11 +22,14 @@
 <script>
 import products from "@/products.json";
 import Product from "@/components/Product.vue";
+import Cart from "@/components/Cart.vue";
 
 export default {
 	name: "app",
 	components: {
-		Product
+		// register every component as a component here
+		Product,
+		Cart
 	},
 	data() {
 		return {
---
+++ vue-practice-shop/src/components/Cart.vue
@@ -0,0 +1,17 @@
+<template>
+	<ul class="list-group">
+		<li class="list-group-item" v-for="item in items" :key="item.id">
+			{{item.title}} - $ {{item.price}}
+			<button
+				class="btn badge badge-danger float-right"
+			>Remove From Cart</button>
+		</li>
+	</ul>
+</template>
+
+<script>
+export default {
+	props: ["items"]
+};
+</script>
+

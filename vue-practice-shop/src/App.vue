9bb30c0ac277347e4231f17f2fa150f2ffcb69db
Leila haghdoost <leilahaghdoost@192-168-1-6.tpgi.com.au>
2019-02-03 22:32:32 +1100

    Displaying cart total

--- vue-practice-shop/src/App.vue
+++ vue-practice-shop/src/App.vue
@@ -13,7 +13,7 @@
 				</div>
 			</div>
 			<div class="col-md-5 my-5">
-				<cart v-on:remove-from-cart="removeFromCart($event)" :items="cart"></cart>
+				<cart v-on:pay="pay()" v-on:remove-from-cart="removeFromCart($event)" :items="cart"></cart>
 			</div>
 		</div>
 	</div>
@@ -50,6 +50,10 @@ export default {
 		},
 		removeFromCart(product) {
 			this.cart = this.cart.filter(item => item.id !== product.id);
+		},
+		pay() {
+			this.cart = [];
+			alert("Shopping completed!");
 		}
 	}
 };
